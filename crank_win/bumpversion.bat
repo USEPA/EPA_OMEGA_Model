c29b7a2f4babad00f85541d37b5ef2df78ff62de
kevinanewman <newman.kevin@epa.gov>
2019-07-23 13:35:55 -0400

    commit before version bump

--- crank_win/bumpversion.bat
+++ crank_win/bumpversion.bat
@@ -4,6 +4,6 @@ SET PYTHONPATH=C:\Users\Public\OMEGA2\Python3.6.7
 
 cd ..
 
-git commit -m "commit before release" --all
+git commit -m "commit before version bump" --all
 
-%PYTHONPATH%\Scripts\bumpversion %1
+%PYTHONPATH%\Scripts\bumpversion %1 --verbose
