9f9ce8f7bf1593754c3006b483ae0aa1511cc46e
kevinanewman <newman.kevin@epa.gov>
2019-07-23 08:07:49 -0400

    Added files to create a runable module and an initial PyPi package

---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.rst").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="usepa-omega2",
+    version="0.0.1",
+    description="OMEGA Model, version 2",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/USEPA/EPA_OMEGA_Model",
+    author="US EPA",
+    author_email="newman.kevin@epa.gov",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["omega2"],                # or something like packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["numpy", "matplotlib"],
+    entry_points={
+        "console_scripts": [
+            "omega2=omega2.__main__:main",
+        ]
+    },
+)
