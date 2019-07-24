31712f2d9a94fbaaea17a5010066102be603a4a6
kevinanewman <newman.kevin@epa.gov>
2019-07-24 10:36:19 -0400

    Bump version: 0.1.9 → 0.1.10

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, US EPA'
 author = 'US EPA'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.9'
+release = '0.1.10'
 
 
 # -- General configuration ---------------------------------------------------
