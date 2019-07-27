1383ad43529eb9c5b48c2cd60268aee3e5e924f5
tlsherwood <toddsherwood@outlook.com>
2019-07-27 13:19:38 -0400

    random outside human has forked repo and fixed a typo

--- usepa_omega2/__main__.py
+++ usepa_omega2/__main__.py
@@ -12,5 +12,5 @@ import usepa_omega2
 
 if __name__ == "__main__":
 
-    print('OMEGA2 greeets you, version %s' % usepa_omega2.__version__)
+    print('OMEGA2 greets you, version %s' % usepa_omega2.__version__)
     print('from %s with love' % usepa_omega2.get_filenameext(os.path.abspath(__file__)))
