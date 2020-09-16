448791e9468b0e13d569722144bcad6fe1229124
Jeff Cherry <cherry.jeff@epa.gov>
2020-09-16 16:22:51 -0400

    Added Documentation V8

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.join(path, '..', '..', 'gui'))
 # -- Project information -----------------------------------------------------
 
 project = 'OMEGA2'
-copyright = '2019, US EPA'
+copyright = '2020, US EPA'
 author = 'US EPA'
 
 # The full version, including alpha/beta/rc tags
