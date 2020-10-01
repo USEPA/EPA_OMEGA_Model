7a577161f2d38c7e8638c83f6a4649f5402a5c99
kevinanewman <newman.kevin@epa.gov>
2020-10-01 12:04:23 -0400

    removed incorrect comments in bump_version.bat
    Signed-off-by: kevinanewman <newman.kevin@epa.gov>

--- crank_win/bump_version.bat
+++ crank_win/bump_version.bat
@@ -1,15 +1,12 @@
 REM bump_version [major|minor|patch]
-REM or
-REM bump_version --new-version x.y.z
-REM or any other valid bumpversion argument
 
 cd ..
 
 REM auto-commit any changes (might remove this in the future) otherwise bumpversion will fail if there are
 REM     outstanding commits
-git commit -m "commit before version bump" --all
+REM git commit -m "commit before version bump" --all
 
 REM bump the version
-bumpversion %1 %2 --verbose --dry_run
+bumpversion %1 --verbose
 
 cd crank_win
