bbd1d069cf470e3577ce6943d79ae5ac5f7e6637
Todd Sherwood <sherwood.todd@epa.gov>
2020-12-03 11:30:52 -0500

    Rework of o2_effects.py and inventory.py to mirror approach used in stock.py. Cleaned out legacy (slow) code from stock.py. Inventories are now written to vehicle_annual_data table.

--- usepa_omega2/effects/o2_effects.py
+++ usepa_omega2/effects/o2_effects.py
@@ -2,33 +2,18 @@
 
 o2_effects.py
 """
-from itertools import product
+
 
 import o2
 from usepa_omega2 import *
-# from vehicles import VehicleFinal
-# from vehicle_annual_data import VehicleAnnualData
-# from effects.inventory import VehicleInventory
-# from effects.emission_factors_vehicles import EmissionFactorsVehicles
-from effects.inventory import calc_vehicle_co2_gallons, calc_vehicle_inventory, calc_refinery_inventory, calc_powersector_inventory
+from effects.inventory import calc_inventory
 
 
-vehicle_criteria_factors = ['voc', 'co', 'nox', 'pm25', 'sox', 'benzene', 'butadiene13', 'formaldehyde', 'acetaldehyde', 'acrolein']
-vehicle_ghg_factors = ['ch4', 'n2o']
-vehicle_factors = vehicle_criteria_factors + vehicle_ghg_factors
+def run_effects_calcs():
+    from vehicle_annual_data import VehicleAnnualData
+    calendar_years = sql_unpack_result(o2.session.query(VehicleAnnualData.calendar_year).all())
+    calendar_years = pd.Series(calendar_years).unique()
 
-powersector_factors = ['voc', 'co', 'nox', 'pm25', 'sox', 'benzene', 'butadiene13', 'formaldehyde', 'acetaldehyde', 'acrolein',
-                       'ch4', 'n2o', 'co2']
-refinery_factors = powersector_factors + ['naphthalene']
-
-
-# def df_from_db_table(table_name):
-#     return pd.read_sql_table(table_name, con=o2.engine)
-
-
-def run_effects_calcs(vf_df, vad_df, vef_df, ref_df, pef_df):
-    inventories = calc_vehicle_co2_gallons(vf_df, vad_df)
-    inventories = calc_vehicle_inventory(inventories, vf_df, vef_df, *vehicle_factors)
-    inventories = calc_refinery_inventory(inventories, ref_df, *refinery_factors)
-    inventories = calc_powersector_inventory(inventories, pef_df, *powersector_factors)
-    return inventories
+    for calendar_year in calendar_years:
+        print(f'Calculating inventories for {calendar_year}')
+        calc_inventory(calendar_year)
