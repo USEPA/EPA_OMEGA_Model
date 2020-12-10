00aa0af8874c53ee502fcb85df1011d9eae71564
Todd Sherwood <sherwood.todd@epa.gov>
2020-12-10 10:38:14 -0500

    emission_costs.py renamed to social_costs.py. Adding fuel costs and energy security calcs.

--- usepa_omega2/effects/social_costs.py
+++ usepa_omega2/effects/social_costs.py
@@ -13,6 +13,22 @@ from usepa_omega2 import *
 vehicles_dict = dict()
 scf_dict = dict()
 ccf_dict = dict()
+fp_dict = dict()
+es_dict = dict()
+
+
+def get_vehicle_info(vehicle_ID, query=False):
+    from vehicles import VehicleFinal
+
+    if vehicle_ID in vehicles_dict and not query:
+        reg_class_ID, in_use_fuel_ID = vehicles_dict[vehicle_ID]
+    else:
+        reg_class_ID, in_use_fuel_ID = o2.session.query(VehicleFinal.reg_class_ID, VehicleFinal.in_use_fuel_ID).\
+            filter(VehicleFinal.vehicle_ID == vehicle_ID).one()
+
+        vehicles_dict[vehicle_ID] = reg_class_ID, in_use_fuel_ID
+
+    return reg_class_ID, in_use_fuel_ID
 
 
 def get_scc_cf(calendar_year, query=False):
@@ -94,18 +110,52 @@ def get_criteria_cf(calendar_year, query=False):
     return pm25_low_3, pm25_high_3, nox_low_3, nox_high_3, pm25_low_7, pm25_high_7, nox_low_7, nox_high_7
 
 
+def get_fuel_prices(calendar_year, fuel_id, query=False):
+    from context_fuel_prices import ContextFuelPrices
+
+    fp_dict_id = f'{calendar_year}_{fuel_id}'
+
+    if fp_dict_id in fp_dict and not query:
+        retail, pretax = fp_dict[fp_dict_id]
+    else:
+        retail, pretax = o2.session.query(ContextFuelPrices.retail_dollars_per_unit,
+                                          ContextFuelPrices.pretax_dollars_per_unit). \
+            filter(ContextFuelPrices.calendar_year == calendar_year).\
+            filter(ContextFuelPrices.fuel_ID == fuel_id).one()
+
+        fp_dict[fp_dict_id] = retail, pretax
+
+    return retail, pretax
+
+
+def get_energysecurity_cf(calendar_year, query=False):
+    from effects.cost_factors_energysecurity import CostFactorsEnergySecurity
+
+    es_dict_id = f'{calendar_year}'
+
+    if es_dict_id in es_dict and not query:
+        es_cf = es_dict[es_dict_id]
+    else:
+        es_cf = o2.session.query(CostFactorsEnergySecurity.dollars_per_gallon). \
+            filter(CostFactorsEnergySecurity.calendar_year == calendar_year).scalar()
+
+        es_dict[es_dict_id] = es_cf
+
+    return es_cf
+
+
+
 def calc_carbon_emission_costs(calendar_year):
     """
     Calculate social costs associated with carbon emissions by calendar year for vehicles in the vehicle_annual_data table.
     :param calendar_year: calendar year
-    :return: Fills data in the monetized effects data table that is empty at this point.
+    :return: Fills data in the cost effects scc data table that is empty at this point.
     """
     from vehicle_annual_data import VehicleAnnualData
     from effects.cost_effects_scc import CostEffectsSCC
 
     query = False
 
-    # vad_vehs = o2.session.query(VehicleAnnualData).filter(VehicleAnnualData.calendar_year == calendar_year).all()
     vad_vehs = o2.session.query(VehicleAnnualData.vehicle_ID,
                                 VehicleAnnualData.age,
                                 VehicleAnnualData.co2_total_metrictons,
@@ -113,10 +163,10 @@ def calc_carbon_emission_costs(calendar_year):
                                 VehicleAnnualData.n2o_vehicle_metrictons).\
         filter(VehicleAnnualData.calendar_year == calendar_year).all()
 
-    # UPDATE monetized effects data
+    # UPDATE cost effects data
     # Since the monetized effects data table is empty, the med_list will store all data for this calendar year
     # and write to that table in bulk via the add all.
-    med_list = list()
+    ed_list = list()
     for vad_veh in vad_vehs:
         # get tons
         vehicle_ID, age, co2_tons, ch4_tons, n2o_tons = vad_veh[0], vad_veh[1], vad_veh[2], vad_veh[3], vad_veh[4]
@@ -154,53 +204,52 @@ def calc_carbon_emission_costs(calendar_year):
         n2o_global_30_social_cost_dollars = n2o_tons * n2o_global_30
         n2o_global_70_social_cost_dollars = n2o_tons * n2o_global_70
 
-        med_list.append(CostEffectsSCC(vehicle_ID = vehicle_ID,
-                                       calendar_year = calendar_year,
-                                       age = age,
-                                       discount_status = 'undiscounted',
-                                       co2_domestic_25_social_cost_dollars = co2_domestic_25_social_cost_dollars,
-                                       co2_domestic_30_social_cost_dollars = co2_domestic_30_social_cost_dollars,
-                                       co2_domestic_70_social_cost_dollars = co2_domestic_70_social_cost_dollars,
-                                       ch4_domestic_25_social_cost_dollars = ch4_domestic_25_social_cost_dollars,
-                                       ch4_domestic_30_social_cost_dollars = ch4_domestic_30_social_cost_dollars,
-                                       ch4_domestic_70_social_cost_dollars = ch4_domestic_70_social_cost_dollars,
-                                       n2o_domestic_25_social_cost_dollars = n2o_domestic_25_social_cost_dollars,
-                                       n2o_domestic_30_social_cost_dollars = n2o_domestic_30_social_cost_dollars,
-                                       n2o_domestic_70_social_cost_dollars = n2o_domestic_70_social_cost_dollars,
-                                       co2_global_25_social_cost_dollars = co2_global_25_social_cost_dollars,
-                                       co2_global_30_social_cost_dollars = co2_global_30_social_cost_dollars,
-                                       co2_global_70_social_cost_dollars = co2_global_70_social_cost_dollars,
-                                       ch4_global_25_social_cost_dollars = ch4_global_25_social_cost_dollars,
-                                       ch4_global_30_social_cost_dollars = ch4_global_30_social_cost_dollars,
-                                       ch4_global_70_social_cost_dollars = ch4_global_70_social_cost_dollars,
-                                       n2o_global_25_social_cost_dollars = n2o_global_25_social_cost_dollars,
-                                       n2o_global_30_social_cost_dollars = n2o_global_30_social_cost_dollars,
-                                       n2o_global_70_social_cost_dollars = n2o_global_70_social_cost_dollars,
-                                       )
+        ed_list.append(CostEffectsSCC(vehicle_ID = vehicle_ID,
+                                      calendar_year = calendar_year,
+                                      age = age,
+                                      discount_status = 'undiscounted',
+                                      co2_domestic_25_social_cost_dollars = co2_domestic_25_social_cost_dollars,
+                                      co2_domestic_30_social_cost_dollars = co2_domestic_30_social_cost_dollars,
+                                      co2_domestic_70_social_cost_dollars = co2_domestic_70_social_cost_dollars,
+                                      ch4_domestic_25_social_cost_dollars = ch4_domestic_25_social_cost_dollars,
+                                      ch4_domestic_30_social_cost_dollars = ch4_domestic_30_social_cost_dollars,
+                                      ch4_domestic_70_social_cost_dollars = ch4_domestic_70_social_cost_dollars,
+                                      n2o_domestic_25_social_cost_dollars = n2o_domestic_25_social_cost_dollars,
+                                      n2o_domestic_30_social_cost_dollars = n2o_domestic_30_social_cost_dollars,
+                                      n2o_domestic_70_social_cost_dollars = n2o_domestic_70_social_cost_dollars,
+                                      co2_global_25_social_cost_dollars = co2_global_25_social_cost_dollars,
+                                      co2_global_30_social_cost_dollars = co2_global_30_social_cost_dollars,
+                                      co2_global_70_social_cost_dollars = co2_global_70_social_cost_dollars,
+                                      ch4_global_25_social_cost_dollars = ch4_global_25_social_cost_dollars,
+                                      ch4_global_30_social_cost_dollars = ch4_global_30_social_cost_dollars,
+                                      ch4_global_70_social_cost_dollars = ch4_global_70_social_cost_dollars,
+                                      n2o_global_25_social_cost_dollars = n2o_global_25_social_cost_dollars,
+                                      n2o_global_30_social_cost_dollars = n2o_global_30_social_cost_dollars,
+                                      n2o_global_70_social_cost_dollars = n2o_global_70_social_cost_dollars,
+                                      )
                         )
-    o2.session.add_all(med_list)
+    o2.session.add_all(ed_list)
 
 
 def calc_criteria_emission_costs(calendar_year):
     """
     Calculate social costs associated with criteria emissions by calendar year for vehicles in the vehicle_annual_data table.
     :param calendar_year: calendar year
-    :return: Fills data in the vehicle_annual_data table that has not been filled to this point.
+    :return: Fills data in the cost effects criteria table that has not been filled to this point.
     """
     from vehicle_annual_data import VehicleAnnualData
     from effects.cost_effects_criteria import CostEffectsCriteria
 
     query = False
 
-    # vad_vehs = o2.session.query(VehicleAnnualData).filter(VehicleAnnualData.calendar_year == calendar_year).all()
     vad_vehs = o2.session.query(VehicleAnnualData.vehicle_ID,
                                 VehicleAnnualData.age,
                                 VehicleAnnualData.pm25_total_ustons,
                                 VehicleAnnualData.nox_total_ustons,).\
         filter(VehicleAnnualData.calendar_year == calendar_year).all()
 
-    # UPDATE monetized effects data
-    med_list = list()
+    # UPDATE cost effects data
+    ed_list = list()
     for vad_veh in vad_vehs:
         # get tons
         vehicle_ID, age, pm25_tons, nox_tons = vad_veh[0], vad_veh[1], vad_veh[2], vad_veh[3]
@@ -221,37 +270,78 @@ def calc_criteria_emission_costs(calendar_year):
         nox_low_mortality_70_social_cost_dollars = nox_tons * nox_low_7
         nox_high_mortality_70_social_cost_dollars = nox_tons * nox_high_7
 
-        # med_dict = {'pm25_low_mortality_30_social_cost_dollars': pm25_low_mortality_30_social_cost_dollars,
-        #             'pm25_high_mortality_30_social_cost_dollars': pm25_high_mortality_30_social_cost_dollars,
-        #             'nox_low_mortality_30_social_cost_dollars': nox_low_mortality_30_social_cost_dollars,
-        #             'nox_high_mortality_30_social_cost_dollars': nox_high_mortality_30_social_cost_dollars,
-        #             'pm25_low_mortality_70_social_cost_dollars': pm25_low_mortality_70_social_cost_dollars,
-        #             'pm25_high_mortality_70_social_cost_dollars': pm25_high_mortality_70_social_cost_dollars,
-        #             'nox_low_mortality_70_social_cost_dollars': nox_low_mortality_70_social_cost_dollars,
-        #             'nox_high_mortality_70_social_cost_dollars': nox_high_mortality_70_social_cost_dollars,
-        #             }
-
-        # # set med_veh details
-        # med_veh = o2.session.query(MonetizedEffectsData). \
-        #     filter(MonetizedEffectsData.vehicle_ID == vehicle_ID). \
-        #     filter(MonetizedEffectsData.calendar_year == calendar_year). \
-        #     filter(MonetizedEffectsData.age == age). \
-        #     filter(MonetizedEffectsData.discount_status == 'undiscounted').one()
-        #
-        # MonetizedEffectsData.update_undiscounted_monetized_effects_data(med_veh, med_dict)
-
-        med_list.append(CostEffectsCriteria(vehicle_ID = vehicle_ID,
-                                            calendar_year = calendar_year,
-                                            age = age,
-                                            discount_status = 'undiscounted',
-                                            pm25_low_mortality_30_social_cost_dollars = pm25_low_mortality_30_social_cost_dollars,
-                                            pm25_high_mortality_30_social_cost_dollars = pm25_high_mortality_30_social_cost_dollars,
-                                            nox_low_mortality_30_social_cost_dollars = nox_low_mortality_30_social_cost_dollars,
-                                            nox_high_mortality_30_social_cost_dollars = nox_high_mortality_30_social_cost_dollars,
-                                            pm25_low_mortality_70_social_cost_dollars = pm25_low_mortality_70_social_cost_dollars,
-                                            pm25_high_mortality_70_social_cost_dollars = pm25_high_mortality_70_social_cost_dollars,
-                                            nox_low_mortality_70_social_cost_dollars = nox_low_mortality_70_social_cost_dollars,
-                                            nox_high_mortality_70_social_cost_dollars = nox_high_mortality_70_social_cost_dollars,
-                                            )
+        ed_list.append(CostEffectsCriteria(vehicle_ID = vehicle_ID,
+                                           calendar_year = calendar_year,
+                                           age = age,
+                                           discount_status = 'undiscounted',
+                                           pm25_low_mortality_30_social_cost_dollars = pm25_low_mortality_30_social_cost_dollars,
+                                           pm25_high_mortality_30_social_cost_dollars = pm25_high_mortality_30_social_cost_dollars,
+                                           nox_low_mortality_30_social_cost_dollars = nox_low_mortality_30_social_cost_dollars,
+                                           nox_high_mortality_30_social_cost_dollars = nox_high_mortality_30_social_cost_dollars,
+                                           pm25_low_mortality_70_social_cost_dollars = pm25_low_mortality_70_social_cost_dollars,
+                                           pm25_high_mortality_70_social_cost_dollars = pm25_high_mortality_70_social_cost_dollars,
+                                           nox_low_mortality_70_social_cost_dollars = nox_low_mortality_70_social_cost_dollars,
+                                           nox_high_mortality_70_social_cost_dollars = nox_high_mortality_70_social_cost_dollars,
+                                           )
                         )
-    o2.session.add_all(med_list)
+    o2.session.add_all(ed_list)
+
+
+def calc_non_emission_costs(calendar_year): # TODO congestion/noise/other?
+    """
+    Calculate social costs associated with fuel consumption by calendar year for vehicles in the vehicle_annual_data table.
+    :param calendar_year: calendar year
+    :return: Fills data in the cost effects non-emissions table that has not been filled to this point.
+    """
+    from vehicle_annual_data import VehicleAnnualData
+    from effects.cost_effects_non_emissions import CostEffectsNonEmissions
+
+    query = False
+
+    vad_vehs = o2.session.query(VehicleAnnualData.vehicle_ID,
+                                VehicleAnnualData.age,
+                                VehicleAnnualData.fuel_consumption,
+                                VehicleAnnualData.vmt, ). \
+        filter(VehicleAnnualData.calendar_year == calendar_year).all()
+
+    # UPDATE cost effects data
+    ed_list = list()
+    for vad_veh in vad_vehs:
+        # get vehicle annual data
+        vehicle_ID, age, fuel_consumption, vmt = vad_veh[0], vad_veh[1], vad_veh[2], vad_veh[3]
+
+        # get vehicle final data
+        reg_class_ID, in_use_fuel_ID = get_vehicle_info(vehicle_ID, query=query)
+
+        # get fuel prices
+        retail, pretax = get_fuel_prices(calendar_year, in_use_fuel_ID, query=query)
+
+        # fuel costs
+        fuel_30_retail_cost_dollars = fuel_consumption * retail
+        fuel_70_retail_cost_dollars = fuel_consumption * retail
+        fuel_30_social_cost_dollars = fuel_consumption * pretax
+        fuel_70_social_cost_dollars = fuel_consumption * pretax
+
+        # get energy security cost factors
+        es_cost_factor = get_energysecurity_cf(calendar_year, query=query)
+
+        # energy security
+        if in_use_fuel_ID == 'pump gasoline':
+            energy_security_30_social_cost_dollars = fuel_consumption * es_cost_factor
+            energy_security_70_social_cost_dollars = fuel_consumption * es_cost_factor
+        else:
+            energy_security_30_social_cost_dollars, energy_security_70_social_cost_dollars = 0, 0
+
+        ed_list.append(CostEffectsNonEmissions(vehicle_ID=vehicle_ID,
+                                               calendar_year=calendar_year,
+                                               age=age,
+                                               discount_status='undiscounted',
+                                               fuel_30_retail_cost_dollars=fuel_30_retail_cost_dollars,
+                                               fuel_70_retail_cost_dollars=fuel_70_retail_cost_dollars,
+                                               fuel_30_social_cost_dollars=fuel_30_social_cost_dollars,
+                                               fuel_70_social_cost_dollars=fuel_70_social_cost_dollars,
+                                               energy_security_30_social_cost_dollars=energy_security_30_social_cost_dollars,
+                                               energy_security_70_social_cost_dollars=energy_security_70_social_cost_dollars,
+                                               )
+                        )
+    o2.session.add_all(ed_list)
