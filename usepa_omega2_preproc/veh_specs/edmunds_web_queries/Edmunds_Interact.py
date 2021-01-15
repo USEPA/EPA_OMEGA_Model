64781c74f0b1aad67dcb7b974393c0063a694211
KBolon <bolon.kevin@epa.gov>
2021-01-15 15:09:09 -0500

    Updates to Edmunds web query

--- usepa_omega2_preproc/veh_specs/edmunds_web_queries/Edmunds_Interact.py
+++ usepa_omega2_preproc/veh_specs/edmunds_web_queries/Edmunds_Interact.py
@@ -8,19 +8,23 @@ from selenium.common.exceptions import WebDriverException, NoSuchElementExceptio
 from selenium.webdriver.common.action_chains import ActionChains
 
 import os
+import time
 import pandas as pd
 import math
 from bs4 import BeautifulSoup
 import numpy as np
+
+
 # import signal
 
 class HTMLTableParser:
     def get_html_table(self, soup, trim_group, errorflag_1, errorflag_2, errorflag_3):
-        full_dataframe_list =  [self.parse_html_table(table, trim_group, errorflag_1, errorflag_2, errorflag_3) \
-                                for table in soup.find_all('table') \
-                                if len(table.find_all('tr')) > 1 and pd.Series(table.get('class')).str.cat(sep=' ') \
-                                != 'features-table table-sm mt-lg-3 w-100-up-md'] #table['id']
+        full_dataframe_list = [self.parse_html_table(table, trim_group, errorflag_1, errorflag_2, errorflag_3) \
+                               for table in soup.find_all('table') \
+                               if len(table.find_all('tr')) > 1 and pd.Series(table.get('class')).str.cat(sep=' ') \
+                               != 'features-table table-sm mt-lg-3 w-100-up-md']  # table['id']
         return full_dataframe_list
+
     def parse_html_table(self, table, trim_group, errorflag_1, errorflag_2, errorflag_3):
         n_columns = 0
         n_rows = 0
@@ -55,9 +59,9 @@ class HTMLTableParser:
         for row in table.find_all('tr'):
             column_marker = 0
             columns = row.find_all('td')
-            for column_count in range (0,len(columns)):
+            for column_count in range(0, len(columns)):
                 column = columns[column_count]
-                bool_check_true = column.find_all('span', {'class':'feature-container bool-true'})
+                bool_check_true = column.find_all('span', {'class': 'feature-container bool-true'})
                 bool_check_false = column.find_all('span', {'class': 'feature-container bool-false'})
                 if len(bool_check_true) > 0:
                     df.iat[row_marker, column_marker] = bool(True)
@@ -71,29 +75,83 @@ class HTMLTableParser:
 
         return df
 
+
 def Edmunds_Interact(url):
     max_attempts = 10
     max_time = 60
-    for geturl_attempt in range (0,max_attempts):
-        print('URL Attempt '+str(geturl_attempt+1))
+    trim_options = []
+    for geturl_attempt in range(0, max_attempts):
+        print('URL Attempt ' + str(geturl_attempt + 1))
         # time = (max_time)*(0.125 + ((geturl_attempt+1)/max_attempts))
-        time  = 45
+        time = 45
         chromedriver = 'chromedriver.exe'
+
         os.environ["webdriver.chrome.driver"] = chromedriver
         chromeOptions = Options()
-        #chromeOptions.add_argument("--kiosk")
+        # chromeOptions.add_argument("--kiosk")
         chromeOptions.add_argument("--start-maximized")
-        driver = webdriver.Chrome(chromedriver, chrome_options=chromeOptions)
+        driver = webdriver.Chrome(executable_path=chromedriver)
+        driver.maximize_window()
+        driver.implicitly_wait(5)
         try:
             driver.set_page_load_timeout(time)
             driver.get(url)
             # find and click main button to reveal drop-down menu
             menu1 = driver.find_elements_by_xpath("//button[@data-test='select-menu']")[0]
             menu1.click()
+
+            # menus1 = driver.find_elements_by_xpath("//select[@class = 'style-select h5 mb-0 w-100 bg-white font-weight-bold' and @data-fas-column = '1']")
+            # for menu in menus1:
+            #     if menu.is_displayed():
+            #         menu1 = menu
+            #         break
+            # dropdown1 = Select(menu1)
+            # # signal.alarm(0)
+
             # find trim buttons in drop-down menu
-            trim_buttons = driver.find_elements_by_xpath("//div[@class='dropdown-menu show']//button[@class='dropdown-item']")
-            # collapse drop-down menu
-            menu1.click()
+            trim_buttons = driver.find_elements_by_xpath("//div[@class='dropdown-menu show']/button[@class='dropdown-item']")
+
+            # find trim buttons in drop-down menu
+            trim_buttons = driver.find_elements_by_xpath("//div[@class='dropdown-menu show']/button[@class='dropdown-item']")
+            # menu1.click()
+            for i, trim in zip(range(len(trim_buttons)), trim_buttons):
+                print(trim.text)
+                trim_options.append(trim.text)
+
+            option_text = trim_options[3]
+
+            # # https://stackoverflow.com/questions/50354157/select-from-a-dropdown-box-without-using-the-select-or-option-tags-selenium-pyt
+            # # menu2 = driver.find_elements_by_xpath("//button[@data-test='select-menu']")[1]
+            # # menu2.click()
+            # Dropdown_Element = driver.find_element_by_xpath("//button[text()='{}']".format(str(option_text)))
+            # actions = ActionChains(driver)
+
+            # # Click on the element using the click(on_element=)
+            # actions.click(on_element=Dropdown_Element)
+            # driver.implicitly_wait(2)
+            # #actions.move_to_element(Dropdown_Element)
+
+            # actions.perform()
+            # actions.reset_actions()
+
+            # #ActionChains = 4
+            # menu1.click()
+            # actions = ActionChains(driver)
+            # option_text = trim_options[4]
+            # dropdown_element = driver.find_element_by_xpath("//button[text()='{}']".format(str(option_text)))
+
+            # # Click on the element using the click(on_element=)
+            # actions.click(on_element=dropdown_element)
+            # driver.implicitly_wait(2)
+            # actions.perform()
+
+            # webdriver.ActionChains(driver).move_to_element(Dropdown_Element).click(Dropdown_Element).perform()
+
+            # https://stackoverflow.com/questions/57648236/how-to-handle-a-non-select-dropdown-menu-in-selenium-with-dynamic-class-value
+            # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[starts-with(@class, 'responsiveDropdownMenu__title--')]//following::span[starts-with(@class, 'responsiveDropdownMenu__label--')]"))).click()
+            # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[starts-with(@class, 'responsiveDropdownMenu__menu--')]//a[@id='desc__ratings_count']"))).click()
+
+            # https://www.lambdatest.com/blog/how-to-get-text-of-an-element-in-selenium/
             break
         except (NoSuchElementException, TimeoutException, UnboundLocalError):
             driver.quit()
@@ -115,15 +173,16 @@ def Edmunds_Interact(url):
         # dropdown3 = Select(menu3)
         trim_group_length = 3
 
-    trim_groups_count = math.ceil(total_trims / 3) #Number of trim groups (in groups of 3, add one if only 1-2 trims left)
-    last_group_count = total_trims % 3 #Length of last group
-    readin_check = pd.Series(np.zeros(total_trims), name = 'Readin_Error').replace(0,'')
+    trim_groups_count = math.ceil(total_trims / 3)  # Number of trim groups (in groups of 3, add one if only 1-2 trims left)
+    last_group_count = total_trims % 3  # Length of last group
+    readin_check = pd.Series(np.zeros(total_trims), name='Readin_Error').replace(0, '')
     for table_list_count in range(0, 1):
         if table_list_count == 0:
             save_name = 'pandas list.csv'
         else:
             save_name = 'soup list.csv'
-        for trim_group in range(0,trim_groups_count):
+        for trim_group in range(0, trim_groups_count):
+            _index = trim_group
             errorflag_1 = 0
             errorflag_2 = 0
             errorflag_3 = 0
@@ -131,35 +190,35 @@ def Edmunds_Interact(url):
                 del important_tables
             except NameError:
                 pass
-            if trim_group == trim_groups_count - 1 and last_group_count != 0: #Last (or only) trim group and less than 3 entries
-                trims = pd.Series(range(last_group_count)) + trim_group_length*trim_group
+            if trim_group == trim_groups_count - 1 and last_group_count != 0:  # Last (or only) trim group and less than 3 entries
+                trims = pd.Series(range(last_group_count)) + trim_group_length * trim_group
             else:
-                trims = pd.Series(range(trim_group_length)) + trim_group_length*trim_group
+                trims = pd.Series(range(trim_group_length)) + trim_group_length * trim_group
             if len(trims) >= 1:
                 try:
                     menu1.click()
-                    #Choose first option
-                    driver.find_elements_by_xpath("//div[@class='dropdown-menu show']//button[@class='dropdown-item']")[0].click()
+                    # Choose first option
+                    driver.find_elements_by_xpath("//div[@class='dropdown-menu show']//button[@class='dropdown-item']")[_index * 3 + 0].click()
                     # dropdown1.select_by_index(trims[0]) #Choose first option
                     # element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.LINK_TEXT, (dropdown1.options)[trims[0]].text)))
                 except TimeoutException:
                     errorflag_1 = 1
                     readin_check[trims[0]] = 'READIN_ERROR'
-            if len(trims) >= 2: #Choose second option (if applicable)
+            if len(trims) >= 2:  # Choose second option (if applicable)
                 try:
                     menu2.click()
                     # Choose second option
-                    driver.find_elements_by_xpath("//div[@class='dropdown-menu show']//button[@class='dropdown-item']")[1].click()
+                    driver.find_elements_by_xpath("//div[@class='dropdown-menu show']//button[@class='dropdown-item']")[_index * 3 + 1].click()
                     # dropdown2.select_by_index(trims[1])
                     # element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.LINK_TEXT, (dropdown2.options)[trims[1]].text)))
                 except TimeoutException:
                     errorflag_2 = 1
                     readin_check[trims[1]] = 'READIN_ERROR'
-            if len(trims) >= 3: #Choose third option (if applicable)
+            if len(trims) >= 3:  # Choose third option (if applicable)
                 try:
                     menu3.click()
                     # Choose third option
-                    driver.find_elements_by_xpath("//div[@class='dropdown-menu show']//button[@class='dropdown-item']")[2].click()
+                    driver.find_elements_by_xpath("//div[@class='dropdown-menu show']//button[@class='dropdown-item']")[_index * 3 + 2].click()
                     # dropdown3.select_by_index(trims[2])
                     # element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.LINK_TEXT, (dropdown3.options)[trims[2]].text)))
                 except TimeoutException:
@@ -174,7 +233,9 @@ def Edmunds_Interact(url):
             # table_list[16].to_csv(
             #     '/Users/Brandon/PycharmProjects/Web_Scraping' + '//' + str(trim_group) + ' ' + save_name,
             #     index=False)
-            for table_count in range(0,len(table_list)):
+            for table_count in range(0, len(table_list)):
+                if table_count == 12:
+                    print(raw_table)
                 if table_list_count == 0 and len(table_list[table_count]) > 3:
                     table_check = 1
                     raw_table = table_list[table_count].reset_index(drop=True)
@@ -186,37 +247,69 @@ def Edmunds_Interact(url):
                     pass
                 if table_check == 1:
                     table_tag = raw_table.columns[0]
+
                     if len(raw_table) > 0 and isinstance(raw_table, pd.DataFrame) and \
-                            not("Highlights" in table_tag)  \
+                            not ("Highlights" in table_tag) \
                             and table_tag != 'OVERVIEW':
                         raw_table.columns = pd.Series(raw_table.columns).str.strip()
-                        for col_count in range(1,len(raw_table.columns)):
+                        # print(raw_table.columns)
+
+                        for col_count in range(1, len(raw_table.columns)):
                             column = raw_table.columns[col_count]
                             msrp = column[column.find('$'):].strip()
-                            raw_table = raw_table.rename\
-                                (columns = {column:column[column.rfind(')',0,column.rfind(')')):column.rfind(')')+1].strip()\
-                                 .replace('  ','').strip() + ' ' + msrp})
+                            # msrp=msrp.split(" ")[0]; msrp=msrp.strip('$'); msrp=msrp.replace(',', '')
+                            raw_table = raw_table.rename \
+                                (columns={column: column[column.rfind(')', 0, column.rfind(')')):column.rfind(')') + 1].strip() \
+                                 .replace('  ', '').strip() + ' ' + msrp})
                         # raw_table.to_csv('/Users/Brandon/PycharmProjects/Web_Scraping/'+'Raw Table.csv')
                         try:
                             important_tables = pd.concat([important_tables, raw_table])
                         except NameError:
                             important_tables = raw_table
-            important_tables = important_tables.replace(np.nan, '').astype(str).applymap(lambda x:x.strip()).reset_index(drop=True)
+            important_tables = important_tables.replace(np.nan, '').astype(str).applymap(lambda x: x.strip()).reset_index(drop=True)
+
+            df = important_tables.copy()
+            df.rename(columns={table_list[0].columns[0]: 'Specifications'}, inplace=True)
+            # df.at[7, 'Category'] = df.at[7, 'Drivetrain']
+            df.insert(0, 'Category', '')
+
+            k = 0;
+            for i in range(len(table_list)):
+                tmp_table = table_list[i]
+                tmp_table_cname = table_list[i].columns[0]
+                df.at[k, df.columns[0]] = tmp_table_cname
+                if i == 0:
+                    df.at[k, df.columns[1]] = tmp_table[tmp_table_cname][0]
+                    df.drop(table_list[1].columns[0], axis=1, inplace=True)
+                elif tmp_table_cname != 'Colors':
+                    for j in range(len(tmp_table)):
+                        df.at[j + k, df.columns[1]] = tmp_table[tmp_table_cname][j]
+                        for ij in range(3):
+                            df.at[j + k, df.columns[ij + 2]] = df.at[j + k, df.columns[ij + 5]]
+                if tmp_table_cname != 'Colors': k += len(tmp_table)
+
+            for i in range(len(trim_options)):
+                df.rename(columns={df.columns[i + 2]: trim_options[i]}, inplace=True)
+
+            df.drop(df.columns[2 + len(trim_options):len(df.columns)], axis=1, inplace=True)
+            df.replace(np.nan, '', inplace=True)
+
             # important_tables.to_csv('/Users/Brandon/PycharmProjects/Web_Scraping/' + 'Important Tables.csv')
-            for s in range (min(3,total_trims),important_tables.shape[1]):
-                if s == min(3,total_trims):
-                    category = important_tables.ix[:,s].astype(str)
+            for s in range(min(3, total_trims), important_tables.shape[1]):
+                if s == min(3, total_trims):
+                    # category = important_tables.ix[:,s].astype(str)
+                    category = important_tables.iloc[:, s].astype(str)
                 else:
-                    category = category.str.cat(important_tables.ix[:,s],sep='')
-            category = pd.Series(category, name = 'Category')
-            important_array = pd.concat([important_tables.ix[:,0:min(3, total_trims)],category],axis=1)\
+                    category = category.str.cat(important_tables.iloc[:, s], sep='')
+            category = pd.Series(category, name='Category')
+            important_array = pd.concat([important_tables.iloc[:, 0:min(3, total_trims)], category], axis=1) \
                 .reset_index(drop=True)
             if trim_group == 0:
                 output_array = important_array
             else:
                 merge_array = pd.concat([important_array['Category'], \
-                                         important_array[important_array.columns.difference(output_array.columns)]],axis=1)
+                                         important_array[important_array.columns.difference(output_array.columns)]], axis=1)
                 output_array = pd.merge(output_array, merge_array, on='Category', how='outer')
 
     driver.close()
-    return (output_array, readin_check)
+    return (df, output_array, readin_check)
