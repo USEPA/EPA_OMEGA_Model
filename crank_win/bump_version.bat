dbce8c3b748cf91dc10ce012338206518da2af94
kevinanewman <newman.kevin@epa.gov>
2021-01-29 10:01:11 -0500

    trying to cleanup bumpversion
    Signed-off-by: kevinanewman <newman.kevin@epa.gov>

--- crank_win/bump_version.bat
+++ crank_win/bump_version.bat
@@ -1,6 +1,6 @@
 REM bump_version [major|minor|patch]
 
-cd ..
+REM cd ..
 
 REM auto-commit any changes (might remove this in the future) otherwise bumpversion will fail if there are
 REM     outstanding commits
@@ -9,4 +9,4 @@ REM git commit -m "commit before version bump" --all
 REM bump the version
 bumpversion %1 --verbose
 
-cd crank_win
+REM cd crank_win
