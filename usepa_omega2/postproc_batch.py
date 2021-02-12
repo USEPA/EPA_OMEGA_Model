f1d0b8d2700ae19904ff69b7caade9b140acae02
kevinanewman <newman.kevin@epa.gov>
2021-02-12 10:36:56 -0500

    added sanity check
    Signed-off-by: kevinanewman <newman.kevin@epa.gov>

--- usepa_omega2/postproc_batch.py
+++ usepa_omega2/postproc_batch.py
@@ -22,18 +22,19 @@ def run_postproc(batch_log, batch_summary_filename):
 
     delta_df = summary_df.copy()
 
-    num_analysis_years = delta_df.shape[0] / len(session_names)
+    num_analysis_years = int(delta_df.shape[0] / len(session_names))
 
-    for i, row in enumerate(summary_df.iterrows()):
-        reference_index = int(i % num_analysis_years)
-        delta_df.iloc[i] = summary_df.iloc[i] - summary_df.iloc[reference_index]
+    # sanity check rows per session
+    if num_analysis_years * len(session_names) == delta_df.shape[0]:
+        for i, row in enumerate(summary_df.iterrows()):
+            reference_index = int(i % num_analysis_years)
+            delta_df.iloc[i] = summary_df.iloc[i] - summary_df.iloc[reference_index]
 
-    # restore string values
-    delta_df['session_name'] = sessions
+        # restore string values
+        delta_df['session_name'] = sessions
 
-    delta_df.to_csv(batch_delta_filename, index=False)
+        delta_df.to_csv(batch_delta_filename, index=False)
 
 
 if __name__ == '__main__':
     batch_summary_filename = 'C:\dev\GitHub\EPA_OMEGA_Model\\bundle\\2021_02_11_14_31_30_multiple_session_batch\summary_results.csv'
-
