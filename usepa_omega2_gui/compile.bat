6da08481731accb04445766ec96316e748ecc639
Jeff Cherry <cherry.jeff@epa.gov>
2021-02-22 08:16:38 -0500

    Replace Plot Definition File

--- usepa_omega2_gui/compile.bat
+++ usepa_omega2_gui/compile.bat
@@ -1,2 +1,3 @@
 # pyinstaller --windowed --onefile omega_gui_batch.py
-pyinstaller --windowed omega_gui_batch.py
+# pyinstaller --windowed omega_gui_batch.py
+pyinstaller exe_entry.py --name omega2 --paths usepa_omega2;usepa_omega2_gui --add-data usepa_omega2;usepa_omega2 --add-data usepa_omega2_gui;usepa_omega2_gui --noconfirm --onefile
