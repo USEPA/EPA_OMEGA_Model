9cace046f1bb9ef5443ed30fc92a6dd0fdc89817
kevinanewman <newman.kevin@epa.gov>
2021-02-23 08:57:51 -0500

    multi-threaded gui! tried to fix non-plotting plots but I don't get the concept
    Signed-off-by: kevinanewman <newman.kevin@epa.gov>

--- usepa_omega2_gui/omega_gui_functions.py
+++ usepa_omega2_gui/omega_gui_functions.py
@@ -182,8 +182,11 @@ def test_plot_2(plot_selection, scenario_selection, plot_select_directory_name):
     Returns:
         N/A
     """
+    import os
+    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + os.sep
+
     # df = pandas.read_excel('usepa_omega2_gui/elements/plot_definition.xlsx',index_col=0, sheet_name='plot_definition')
-    df = pandas.read_csv('usepa_omega2_gui/elements/plot_definition.csv', index_col=0)
+    df = pandas.read_csv(path + 'usepa_omega2_gui/elements/plot_definition.csv', index_col=0)
 
     if len(plot_selection) > 0:
         # print(plot_selection)
