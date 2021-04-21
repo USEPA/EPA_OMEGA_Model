036d66be11e8b4fd0e70b1a975699a969ef4eacc
kevinnewman <newman.kevin@epa.gov>
2021-04-21 14:29:23 -0400

    Bump version: 0.5.0 → 0.6.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, US EPA'
 author = 'US EPA'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.6.0'
 
 
 # -- General configuration ---------------------------------------------------
