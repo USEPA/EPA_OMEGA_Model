Added more dummy vehicles for BEVs in various context size classes Fixed a bug in frontier calculation that caused an infinite loop Added reg class chart to output Fixed a potential runtime error in log print statement

usepa_omega2/cost_clouds.py
@@ -81,6 +81,8 @@ class CostCloud(OMEGABase):
 
         """
 
+        import numpy as np
+
         frontier_pts = []
 
         # drop non-numeric columns so dtypes don't become "object"
@@ -92,7 +94,7 @@ class CostCloud(OMEGABase):
         min_frontier_factor = 0
 
         if cloud[x_key].min() != cloud[x_key].max():
-            while pd.notna(idxmin) and (min_frontier_factor <= 0 or allow_upslope):
+            while pd.notna(idxmin) and (min_frontier_factor <= 0 or allow_upslope) and not np.isinf(min_frontier_factor):
                 # calculate frontier factor (more negative is more better) = slope of each point relative
                 # to prior frontier point if frontier_social_affinity_factor = 1.0, else a "weighted" slope
                 cloud['frontier_factor'] = (cloud[y_key] - frontier_pts[-1][y_key]) \
@@ -109,11 +111,15 @@ class CostCloud(OMEGABase):
                                                ** (1 + 1 - o2.options.cost_curve_frontier_affinity_factor)
                     min_frontier_factor = cloud['frontier_factor'].min()
 
-                if len(cloud[cloud['frontier_factor'] == min_frontier_factor]) > 1:
-                    # if multiple points with the same slope, take the one with the highest index (highest x-value)
-                    idxmin = cloud[cloud['frontier_factor'] == min_frontier_factor].index.max()
+                if not np.isinf(min_frontier_factor):
+                    if len(cloud[cloud['frontier_factor'] == min_frontier_factor]) > 1:
+                        # if multiple points with the same slope, take the one with the highest index (highest x-value)
+                        idxmin = cloud[cloud['frontier_factor'] == min_frontier_factor].index.max()
+                    else:
+                        idxmin = cloud['frontier_factor'].idxmin()
                 else:
-                    idxmin = cloud['frontier_factor'].idxmin()
+                    idxmin = cloud['frontier_factor'].idxmax()
+
                 if pd.notna(idxmin) and (allow_upslope or min_frontier_factor <= 0):
                     frontier_pts.append(cloud.loc[idxmin])
 