64f2742a8fb4c9a3f3a6fc3ccc53a6738757055b
kevinnewman <newman.kevin@epa.gov>
2021-05-27 15:28:14 -0400

    sales incentives by "start year", gets rid of repeated rows

--- usepa_omega2/GHG_standards_sales_incentives.py
+++ usepa_omega2/GHG_standards_sales_incentives.py
@@ -19,28 +19,32 @@ class GHGStandardIncentives(OMEGABase):
 
     @staticmethod
     def get_sales_incentive(vehicle):
-        cache_key = int(vehicle.model_year)
-
         incentive_sales_multiplier = 1
 
-        if cache_key in cache:
-            calcs = cache[cache_key]
-            for calc, multiplier in calcs.items():
-                select_attribute, select_value = calc.split(':')
-                if vehicle.__getattribute__(select_attribute) == select_value:
-                    incentive_sales_multiplier = multiplier
+        start_years = cache['start_year']
+        if start_years[start_years <= vehicle.model_year]:
+            cache_key = max(start_years[start_years <= vehicle.model_year])
+
+            if cache_key in cache:
+                calcs = cache[cache_key]
+                for calc, multiplier in calcs.items():
+                    select_attribute, select_value = calc.split(':')
+                    if vehicle.__getattribute__(select_attribute) == select_value:
+                        incentive_sales_multiplier = multiplier
 
         return incentive_sales_multiplier
 
     @staticmethod
     def init_from_file(filename, verbose=False):
+        import numpy as np
+
         cache.clear()
 
         if verbose:
             omega_log.logwrite('\nInitializing database from %s...' % filename)
 
-        input_template_version = 0.1
-        input_template_columns = {'model_year'}
+        input_template_version = 0.2
+        input_template_columns = {'start_year'}
 
         template_errors = validate_template_version_info(filename, input_template_name, input_template_version,
                                                          verbose=verbose)
@@ -52,7 +56,7 @@ class GHGStandardIncentives(OMEGABase):
             template_errors = validate_template_columns(filename, input_template_columns, df.columns, verbose=verbose)
 
             if not template_errors:
-                df = df.set_index('model_year')
+                df = df.set_index('start_year')
                 df = df.drop([c for c in df.columns if 'Unnamed' in c], axis='columns')
 
                 for idx, r in df.iterrows():
@@ -60,6 +64,8 @@ class GHGStandardIncentives(OMEGABase):
                         cache[idx] = dict()
 
                     cache[idx] = r.to_dict()
+
+                cache['start_year'] = np.array(list(cache.keys()))
 
         return template_errors
 
