23894bd06340df0c32988bf9676f71ab81e3099b
kevinnewman <newman.kevin@epa.gov>
2021-06-23 16:08:17 -0400

    Bump version: 0.6.0 → 0.7.0

--- omega_model/__init__.py
+++ omega_model/__init__.py
@@ -12,7 +12,7 @@ Defines class OMEGARuntimeOptions which control an individual simulation session
 """
 
 # OMEGA2 code version number
-code_version = "0.6.0"
+code_version = "0.7.0"
 print('loading omega version %s' % code_version)
 
 import os, sys
