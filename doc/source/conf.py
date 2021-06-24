250172e93e2dd1d01d6edeedb062fbcc6350f3b3
kevinnewman <newman.kevin@epa.gov>
2021-06-24 11:30:13 -0400

    fix for readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(path, '..', '..', 'omega_model'))
+sys.path.insert(0, os.path.join(path, '..', '..', 'omega_gui'))
 sys.path.insert(0, os.path.join(path, '..', '..'))
 
 # -- Project information -----------------------------------------------------
