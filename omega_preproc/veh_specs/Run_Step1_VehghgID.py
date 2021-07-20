fff02a5e7d5a2e69c9f58a111f7f1d16fa76e7e0
SLee02 <lee.soduk@epa.gov>
2021-07-20 18:20:10 -0400

    Updated ESTIMATE_TARGET_COEF_BEST_MTH case 0, 1, 2, and 3.

--- omega_preproc/veh_specs/Run_Step1_VehghgID.py
+++ omega_preproc/veh_specs/Run_Step1_VehghgID.py
@@ -39,7 +39,8 @@ for run_count in range (0,len(run_controller)):
     roadload_coefficient_table_filename = str(run_controller['Roadload Coefficient Table Filename'][run_count])
     test_car_filename_path = str(run_controller['Test Car File Path'][run_count])
     set_roadload_coefficient_table_filename = str(run_controller['tstcar Fuel Economy Test Filename'][run_count])
-    tstcar_MY_errta_filename = str(run_controller['tstcar MY Errta Filename'][run_count])
+    tstcar_MY_errta_filename = str(run_controller['tstCar File Exceptions Table filename'][run_count])
+    tstcar_MY_carline_name_mapping_filename = str(run_controller['tstCar CARLINE_NAME mapping filename'][run_count])
     set_bodyid_to_lineageid = int(run_controller['SetBodyIDtoLineageID'][run_count])
     drivecycle_filenames = str(run_controller['Drive Cycle Filenames'][run_count])
     if ('[' and ']') in drivecycle_filenames:
@@ -80,18 +81,20 @@ for run_count in range (0,len(run_controller)):
         modeltype_exceptions_table = pd.read_csv(input_path+'\\'+ model_type_exceptions_table_filename)
         subconfig_MY_exceptions_table = pd.read_csv(input_path+'\\'+ subconfig_MY_exceptions_table_filename)
         subconfig_sales_exceptions_table = pd.read_csv(input_path+'\\'+ subconfig_sales_exceptions_table_filename)
+        tstcar_MY_exceptions_table = pd.read_csv(test_car_filename_path +'\\'+ tstcar_MY_errta_filename)
     else:
         modeltype_exceptions_table = 'N'
         subconfig_MY_exceptions_table = 'N'
         subconfig_sales_exceptions_table = 'N'
+        tstcar_MY_exceptions_table = 'N'
 
     if bool_run_new_vehghgid == 'y':
         import Subconfig_ModelType_Footprint_Bodyid_Expansion
         Subconfig_ModelType_Footprint_Bodyid_Expansion.Subconfig_ModelType_Footprint_Bodyid_Expansion\
             (root_drive_letter, input_path, footprint_filename, lineageid_mapping_filename, bodyid_filename, \
              bool_run_new_manual_filter, manual_filter_filename, expanded_footprint_filename, subconfig_filename, model_type_filename, vehghg_filename, output_path_vehghgid, footprint_exceptions_table, \
-             modeltype_exceptions_table, subconfig_MY_exceptions_table, subconfig_sales_exceptions_table, model_year, roadload_coefficient_table_filename, set_bodyid_to_lineageid, \
-             drivecycle_filenames, drivecycle_input_filenames, drivecycle_output_filenames, test_car_filename_path, set_roadload_coefficient_table_filename, tstcar_MY_errta_filename)
+             modeltype_exceptions_table, subconfig_MY_exceptions_table, subconfig_sales_exceptions_table, tstcar_MY_exceptions_table, model_year, roadload_coefficient_table_filename, set_bodyid_to_lineageid, \
+             drivecycle_filenames, drivecycle_input_filenames, drivecycle_output_filenames, test_car_filename_path, set_roadload_coefficient_table_filename, tstcar_MY_carline_name_mapping_filename)
         # import Subconfig_Expansion
         # Subconfig_Expansion.Subconfig_Expansion(input_path, subconfig_filename, output_path_intermediate, \
         #                                         expanded_footprint_filename, output_path_vehghgid, vehghg_filename, 2016)
