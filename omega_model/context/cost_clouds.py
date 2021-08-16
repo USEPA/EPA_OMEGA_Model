de67aff1bf8f05d778f96b26cf598568a67dda0d
kevinnewman <newman.kevin@epa.gov>
2021-08-16 10:21:01 -0400

    add todo

--- omega_model/context/cost_clouds.py
+++ omega_model/context/cost_clouds.py
@@ -155,6 +155,8 @@ class CostCloud(OMEGABase):
             # TODO: validate manufacturer, reg classes, fuel ids, etc, etc....
 
             if not template_errors:
+                # TODO: convert costs to Analysis Dollar Basis
+
                 # convert cost clouds into curves and set up cost_curves table...
                 cost_curve_classes = df['cost_curve_class'].unique()
                 # for each cost curve class
