219f449744804670596c63814242358eee69cc8b
kevinnewman <newman.kevin@epa.gov>
2021-08-16 10:52:48 -0400

    OMEGA2 -> OMEGA

--- omega_model/__init__.py
+++ omega_model/__init__.py
@@ -11,7 +11,7 @@ Defines class OMEGARuntimeOptions which control an individual simulation session
 
 """
 
-# OMEGA2 code version number
+# OMEGA code version number
 code_version = "0.8.0"
 print('loading omega version %s' % code_version)
 
