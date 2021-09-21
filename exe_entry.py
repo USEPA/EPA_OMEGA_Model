86590e2298c8aaf7bca24d71fe2e330971223497
kevinanewman <newman.kevin@epa.gov>
2021-09-21 14:46:34 -0400

    added build_exe_dist.bat modified exe_entry.py to allow first successful omega2 executable    - need to figure out how to do dynamic imports for user-definable modules

--- exe_entry.py
+++ exe_entry.py
@@ -14,5 +14,22 @@ pyinstaller exe_entry.py --name omega2 --paths omega_model:omega_gui --add-data 
 # import omega_model
 import omega_gui.omega_gui
 
+# imports for pyinstaller to package user-definable classes...
+import policy.regulatory_classes
+import policy.targets_alternative
+import policy.targets_footprint
+import policy.regulatory_classes_demo_alt0
+import policy.regulatory_classes_demo_alt1
+import policy.offcycle_credits
+import consumer.reregistration_fixed_by_age
+import consumer.annual_vmt_fixed_by_age
+import consumer.sales_share
+import consumer.market_classes
+import producer.producer_generalized_cost
+
+# fix for for pyinstaller multiprocessing issue
+import multiprocessing
+multiprocessing.freeze_support()
+
 # omega_model.run_omega(omega_model.OMEGARuntimeOptions(), standalone_run=True)
 omega_gui.omega_gui.run_gui()
---
+++ versioning/build_exe_dist.bat
@@ -0,0 +1,14 @@
+REM usage build_dist
+
+cd ..
+
+REM build executable
+pyinstaller exe_entry.py ^
+    --name omega2_0.9.0 ^
+    --paths omega_model;omega_gui ^
+    --add-data omega_model;omega_model ^
+    --add-data omega_gui\elements;omega_gui\elements ^
+    --noconfirm ^
+    --onefile
+
+cd versioning
