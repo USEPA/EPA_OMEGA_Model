a88023d8172b1a6f231d828068f3bc0948c929c0
kevinanewman <newman.kevin@epa.gov>
2021-10-19 17:08:21 -0400

    build process refactor

--- versioning/build_exe_dist.bat
+++ versioning/build_exe_dist.bat
@@ -2,12 +2,14 @@ REM usage build_dist
 
 cd ..
 
+# pyinstaller exe_entry.py --name omega2 --paths omega_model;omega_gui --add-data omega_model;omega_model --add-data omega_gui;omega_gui --noconfirm --onefile
+`
 REM build executable
 pyinstaller exe_entry.py ^
     --name OMEGA-1.9.0 ^
     --paths omega_model;omega_gui ^
     --add-data omega_model;omega_model ^
-    --add-data omega_gui\elements;omega_gui\elements ^
+    --add-data omega_gui;omega_gui ^
     --noconfirm ^
     --onefile
 
