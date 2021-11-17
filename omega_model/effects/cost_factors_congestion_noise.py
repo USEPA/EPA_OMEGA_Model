7aabbe63c5000983ec43c2667fca60661b769366
kevinnewman <newman.kevin@epa.gov>
2021-11-17 11:01:45 -0500

    docstring updates

--- omega_model/effects/cost_factors_congestion_noise.py
+++ omega_model/effects/cost_factors_congestion_noise.py
@@ -90,6 +90,18 @@ class CostFactorsCongestionNoise(SQABase, OMEGABase):
 
     @staticmethod
     def init_database_from_file(filename, verbose=False):
+        """
+
+        Initialize class data from input file.
+
+        Args:
+            filename (str): name of input file
+            verbose (bool): enable additional console and logfile output if True
+
+        Returns:
+            List of template/input errors, else empty list on success
+
+        """
         cache.clear()
 
         if verbose:
