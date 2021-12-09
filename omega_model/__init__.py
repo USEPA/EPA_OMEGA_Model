ff6728b20d592ac2c4e58bd8d5dfc8bed867ec52
kevinnewman <newman.kevin@epa.gov>
2021-12-09 13:56:49 -0500

    fixed bug for composite vehicle weighted values

--- omega_model/__init__.py
+++ omega_model/__init__.py
@@ -155,7 +155,7 @@ try:
             self.verbose = False
             self.iterate_producer_consumer = True
 
-            self.producer_consumer_max_iterations = 2  # recommend 2+
+            self.producer_consumer_max_iterations = 20  # recommend 2+
             self.producer_consumer_convergence_tolerance = 5e-4
             self.producer_compliance_search_min_share_range = 1e-5
             self.producer_compliance_search_convergence_factor = 0.5
