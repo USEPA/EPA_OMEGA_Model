79799632bf6cb8b7a7cda913f8f552bfb37b2241
SLee02 <lee.soduk@epa.gov>
2022-04-04 08:18:17 -0400

    Updated for collecting all parameters from the Edmunds.com

--- omega_preproc/veh_specs/edmunds_web_queries/Edmunds_Readin_URLs_Listed.py
+++ omega_preproc/veh_specs/edmunds_web_queries/Edmunds_Readin_URLs_Listed.py
@@ -32,12 +32,12 @@ working_directory = str(Path.home()) + '/Documents/Python/Edmunds_web_vehicle_sp
 run_controller = pd.read_csv(working_directory+'Edmunds Run Controller-2019.csv')
 start_count = 0 #Set to 0 when time permits
 final_table_to_csv_inc = 50 # print final_table csv file at the final_table_to_csv_inc increments
-cols_safety = ["DUAL FRONT SIDE-MOUNTED AIRBAGS", "DUAL FRONT WITH HEAD PROTECTION CHAMBERS SIDE-MOUNTED AIRBAGS",
-                "DUAL FRONT AND DUAL REAR SIDE-MOUNTED AIRBAGS",
-                "DUAL FRONT AND DUAL REAR WITH HEAD PROTECTION CHAMBERS SIDE-MOUNTED AIRBAGS",
-                "DRIVER ONLY WITH HEAD PROTECTION CHAMBER SIDE-MOUNTED AIRBAGS",
-                "FRONT, REAR AND THIRD ROW HEAD AIRBAGS", "FRONT AND REAR HEAD AIRBAGS", "FRONT HEAD AIRBAGS",
-                "STABILITY CONTROL", "TRACTION CONTROL", "TIRE PRESSURE MONITORING"]
+# cols_safety = ["DUAL FRONT SIDE-MOUNTED AIRBAGS", "DUAL FRONT WITH HEAD PROTECTION CHAMBERS SIDE-MOUNTED AIRBAGS",
+#                 "DUAL FRONT AND DUAL REAR SIDE-MOUNTED AIRBAGS",
+#                 "DUAL FRONT AND DUAL REAR WITH HEAD PROTECTION CHAMBERS SIDE-MOUNTED AIRBAGS",
+#                 "DRIVER ONLY WITH HEAD PROTECTION CHAMBER SIDE-MOUNTED AIRBAGS",
+#                 "FRONT, REAR AND THIRD ROW HEAD AIRBAGS", "FRONT AND REAR HEAD AIRBAGS", "FRONT HEAD AIRBAGS",
+#                 "STABILITY CONTROL", "TRACTION CONTROL", "TIRE PRESSURE MONITORING"]
 
 for run_count in range (0,len(run_controller)):
     if run_count > 0: del final_table, reformatted_table
@@ -70,22 +70,28 @@ for run_count in range (0,len(run_controller)):
                 and (final_table['URL']  == url.upper()).sum() == 0 and (final_table['URL']  == url).sum() == 0):
             model = edmunds_models[url_count]
             make = edmunds_makes[url_count]
-            original_output_table, readin_check = Edmunds_Interact.Edmunds_Interact(url)
+            [original_output_table, readin_check, trim_text] = Edmunds_Interact.Edmunds_Interact(url)
             if type(original_output_table) == str:
                 weberror[len(weberror)] = url
                 weberror.to_csv(working_directory + 'Non-Functioning Websites_MY'+str(model_year)+'.csv',index=False)
                 continue
             category_name = original_output_table['Category']
             specification_name = original_output_table['Specifications']
-            original_output_table = original_output_table.drop('Category', axis=1)
-            output_table = original_output_table.drop('Specifications',axis=1)
-            trims_msrp = output_table.columns.str.strip()
-            try:
-                msrp = pd.Series(trims_msrp.str.rsplit('$').str[1].str.strip(), name = 'MSRP')
-            except AttributeError:
-                msrp = pd.Series(np.zeros(len(trims_msrp)), name = 'MSRP').astype(str)
-            trims = pd.Series(trims_msrp.str.rsplit(' - ').str[0].str.strip(), name='Trims')
-            output_table.columns = trims
+            output_table = original_output_table.drop(['Category', 'Specifications'], axis=1)
+            # output_table = original_output_table.drop('Specifications',axis=1)
+            # trims_msrp = output_table.columns.str.strip()
+            # try:
+            #     msrp = pd.Series(trims_msrp.str.rsplit('$').str[1].str.strip(), name = 'MSRP')
+            #     tmp_str = original_output_table['Trim_x'].str.rsplit('$').str[1].str.strip()[0].split(' ')[0]
+            # except AttributeError:
+            #     msrp = pd.Series(np.zeros(len(trims_msrp)), name = 'MSRP').astype(str)
+            trims = []; msrp = []
+            for i in range(len(trim_text)):
+                trims.append((trim_text[i].rsplit(' - ')[0].strip()))
+                msrp.append((trim_text[i].rsplit('$')[1].strip()))
+
+            trims = pd.Series(trims, name='Trims'); msrp = pd.Series(msrp, name='MSRP')
+            # trims = pd.Series(trims.str.rsplit(' - ').str[0].str.strip(), name='Trims')
             new_output_table = output_table.T.reset_index(drop=True)
             new_output_table.columns = specification_name.str.upper()
             make_info = pd.Series(np.zeros(len(new_output_table)),name = 'Make').replace(0,make)
@@ -102,13 +108,17 @@ for run_count in range (0,len(run_controller)):
                 if (pd.Series(reformatted_table.columns) == column).sum() > 1:
                     reformatted_table = reformatted_table.drop(column,axis=1)
             try:
-                non_merge_columns = list(reformatted_table.columns.difference(final_table.columns))
-                merge_columns = list(reformatted_table.columns.difference(non_merge_columns))
+                # non_merge_columns = list(reformatted_table.columns.difference(final_table.columns))
+                # merge_columns = list(reformatted_table.columns.difference(non_merge_columns))
                 reformatted_table = reformatted_table.loc[:, ~reformatted_table.columns.duplicated()]
                 reformatted_table = reformatted_table.dropna(how='all', axis=1)
                 final_table = final_table.loc[:, ~final_table.columns.duplicated()]
-                final_table = final_table.merge(reformatted_table, how='outer', on=merge_columns).sort_values('URL')
-                final_table = final_table.dropna(how='all', axis=1)
+                if len(final_table) > 0:
+                    final_table = final_table.merge(reformatted_table, how='outer').sort_values('URL')
+                else:
+                    final_table = reformatted_table
+                # final_table = final_table.merge(reformatted_table, how='outer', on=specification_name).sort_values('URL')
+                # final_table = final_table.dropna(how='all', axis=1)
             except NameError:
                 reformatted_table = reformatted_table.dropna(how='all', axis=1)
                 final_table = reformatted_table
@@ -139,16 +149,16 @@ for run_count in range (0,len(run_controller)):
     final_table = final_table.fillna('')
     final_table = final_table.reset_index(drop=True)
 
-    cols_final_table = list(final_table)
-    _icols_safety = []
-    for i in range (len(cols_safety)):
-        icol_safety = cols_safety[i]
-        if icol_safety in cols_final_table:
-            _icols_safety += [icol_safety]
-            if len(final_table.loc[final_table[icol_safety] == '']) > 0:
-                final_table[icol_safety] = final_table[icol_safety].replace('', 'null ')
-
-    final_table = movecol(final_table, cols_to_move= _icols_safety, ref_cols = ['CAM TYPE', 'CYLINDER DEACTIVATION'], place='After')
+    # cols_final_table = list(final_table)
+    # _icols_safety = []
+    # for i in range (len(cols_safety)):
+    #     icol_safety = cols_safety[i]
+    #     if icol_safety in cols_final_table:
+    #         _icols_safety += [icol_safety]
+    #         if len(final_table.loc[final_table[icol_safety] == '']) > 0:
+    #             final_table[icol_safety] = final_table[icol_safety].replace('', 'null ')
+    #
+    # final_table = movecol(final_table, cols_to_move= _icols_safety, ref_cols = ['CAM TYPE', 'CYLINDER DEACTIVATION'], place='After')
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
     final_table.to_csv(working_directory + output_name.split('.')[0] + '_' + timestr + '.csv', index=False)
