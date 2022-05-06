7c82f172271e3015b8620458f71c2a047419339a
Kevin Newman <47572615+kevinanewman@users.noreply.github.com>
2022-05-06 11:33:29 -0400

    turn on some logging for debugging

--- omega_model/__init__.py
+++ omega_model/__init__.py
@@ -200,7 +200,7 @@ try:
 
             # list of modules to allow verbose console output, or empty to disable
             self.verbose_console_modules = ['producer_compliance_search_',
-                                            'p-c_shares_and_costs_', 'p-c_max_iterations_',
+                                            'p-c_shares_and_costs', 'p-c_max_iterations_',
                                             'cross_subsidy_search_', 'cross_subsidy_multipliers_',
                                             'cross_subsidy_convergence_']
 
