3373b0dabdef7079ddb128e8db5e67798557e8b0
toddsherwood-EPA <sherwood.todd@epa.gov>
2022-07-25 10:14:25 -0400

    Fix typo in safety_values.csv header. Add documentation to safety_values.py

--- omega_model/effects/safety_values.py
+++ omega_model/effects/safety_values.py
@@ -1,3 +1,56 @@
+"""
+
+**INPUT FILE FORMAT**
+
+The file format consists of a one-row template header followed by a one-row data header and subsequent data
+rows.
+
+The data represent safety values associated with mass reduction in the fleet.
+
+File Type
+    comma-separated values (CSV)
+
+Template Header
+    .. csv-table::
+
+       input_template_name:,safety_values,input_template_version:,0.1
+
+Sample Data Columns
+    .. csv-table::
+        :widths: auto
+
+        body_style,nhtsa_safety_class,threshold_lbs,change_per_100_lbs_below_threshold,change_per_100_lbs_at_or_above_threshold
+        sedan,PC,3201,0.012,0.0042
+        pickup,LT/SUV,5014,0.0031,-0.0061
+        cuv_suv,CUV/Minivan,3872,-0.0025,-0.0025
+
+Data Column Name and Description
+    :body_style:
+        The OMEGA body_style (e.g., sedan, pickup, cuv_suv)
+
+    :nhtsa_safety_class:
+        The NHTSA safety class (e.g., PC, LT/SUV, CUV/Minivan)
+
+    :threshold_lbs:
+        The curb weight, in pounds, above and below which safety values may change.
+
+    :change_per_100_lbs_below_threshold:
+        A percentage change in the fatality rate associated with reductions in curb weight below the associated curb
+        weight threshold; a positive value denotes an increase in fatality rate associated with a reduced curb weight
+        while a negative value denotes a decrease in fatality rate associated with a reduced curb weight; increased
+        curb weights would have the opposite effect on fatality rate.
+
+    :change_per_100_lbs_at_or_above_threshold:
+        A percentage change in the fatality rate associated with reductions in curb weight above the associated curb
+        weight threshold; a positive value denotes an increase in fatality rate associated with a reduced curb weight
+        while a negative value denotes a decrease in fatality rate associated with a reduced curb weight; increased
+        curb weights would have the opposite effect on fatality rate.
+
+----
+
+**CODE**
+
+"""
 from omega_model import *
 
 
@@ -25,7 +78,7 @@ class SafetyValues(OMEGABase):
         """
         threshold = SafetyValues._data[body_style]['threshold_lbs']
         change_below = SafetyValues._data[body_style]['change_per_100_lbs_below_threshold']
-        change_above = SafetyValues._data[body_style]['change_ per_100_lbs_at_or_above_threshold']
+        change_above = SafetyValues._data[body_style]['change_per_100_lbs_at_or_above_threshold']
 
         return threshold, change_below, change_above
 
@@ -55,7 +108,7 @@ class SafetyValues(OMEGABase):
             'nhtsa_safety_class',
             'threshold_lbs',
             'change_per_100_lbs_below_threshold',
-            'change_ per_100_lbs_at_or_above_threshold',
+            'change_per_100_lbs_at_or_above_threshold',
         }
 
         template_errors = validate_template_version_info(filename, input_template_name, input_template_version,
