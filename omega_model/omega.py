3338de108a4d3451b9224ebdc26643dc597975f6
Kevin Newman <47572615+kevinanewman@users.noreply.github.com>
2022-08-16 15:30:02 -0400

    filename fix

--- omega_model/omega.py
+++ omega_model/omega.py
@@ -327,7 +327,7 @@ def run_producer_consumer():
 
         credit_banks[compliance_id].transaction_log.to_csv(
             omega_globals.options.output_folder + omega_globals.options.session_unique_name +
-            '%s_GHG_credit_transactions.csv' % compliance_id, index=False)
+            ' %s_GHG_credit_transactions.csv' % compliance_id, index=False)
 
     iteration_log_df = pd.DataFrame(iteration_log)
 
