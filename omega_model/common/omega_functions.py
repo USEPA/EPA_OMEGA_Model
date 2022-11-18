e387642e56e5de7f4b21b4731fb29b8521b4204c
Kevin Newman <47572615+kevinanewman@users.noreply.github.com>
2022-11-18 18:36:15 -0500

    new and improved print_dict, allows string output or console output

--- omega_model/common/omega_functions.py
+++ omega_model/common/omega_functions.py
@@ -279,33 +279,45 @@ def cull_cloud(cloud, prior_x, x_key):
     return cloud
 
 
-def print_dict(dict_in, num_tabs=0):
+def print_dict(dict_in, num_tabs=0, to_string=False):
     """
     Attempt to printy-print a dictionary to the Python console.
 
     Args:
         dict_in (dict): dictionary to print
         num_tabs (int): optional argument, used to indent subsequent layers of the dictionary
+        to_string (Bool): if True then result will be returned as a printable string, instead of printed to the console
+
+    Returns:
+        print_dict string if to_string==True
 
     """
+    s = ''
+
     if num_tabs == 0:
-        print()
+        s += '\n'
 
     if type(dict_in) is list or type(dict_in) is not dict:
-        print('\t' * num_tabs + str(dict_in))
+        s += '\t' * num_tabs + str(dict_in) + '\n'
     else:
         for k in dict_in.keys():
             if type(dict_in[k]) == list:
                 if dict_in[k]:
-                    print('\t' * num_tabs + str(k) + ':' + str(dict_in[k]))
+                    s += '\t' * num_tabs + str(k) + ':' + str(dict_in[k]) + '\n'
                 else:
-                    print('\t' * num_tabs + str(k))
+                    s += '\t' * num_tabs + str(k) + '\n'
             else:
-                print('\t' * num_tabs + str(k))
-                print_dict(dict_in[k], num_tabs + 1)
+                s += '\t' * num_tabs + str(k) + '\n'
+                s += print_dict(dict_in[k], num_tabs + 1, to_string=True)
 
     if num_tabs == 0:
-        print()
+        s += '\n'
+
+    if not to_string:
+        print(s[:-1])
+
+    if to_string:
+        return s
 
 
 def linspace(min, max, num_values):
