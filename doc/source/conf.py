dd2e9e3a50dcdc7b50c5c7cc9cc69436f0daf5c7
Kevin Newman <47572615+kevinanewman@users.noreply.github.com>
2023-01-20 09:17:13 -0500

    Bump version: 2.0.1 → 2.1.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, US EPA'
 author = 'US EPA'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.1'
+release = '2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
--- omega_model/__init__.py
+++ omega_model/__init__.py
@@ -12,7 +12,7 @@ Defines class OMEGARuntimeOptions which control an individual simulation session
 """
 
 # OMEGA code version number
-code_version = "2.0.1"
+code_version = "2.1.0"
 print('loading omega version %s' % code_version)
 
 import os, sys
