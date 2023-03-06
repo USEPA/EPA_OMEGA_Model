4988a6e2e3d8df719c9b746bce241939c866a433
jcherr01 <cherry.jeff@epa.gov>
2023-03-06 12:14:59 -0500

    Added Polk Analysis

--- testing/polk_analysis.py
+++ testing/polk_analysis.py
@@ -11,11 +11,15 @@ with open('D:/Polk/epa_vmt_202301.csv') as file_obj:
     # Iterate over each row in the csv
     # file using reader object
     a = 0
+    b = 0
     for row in reader_obj:
         a = a + 1
-        # print(row)
-        b = row
-        print(len(b))
-        if a == 30:
+        # b = row
+        # print(b)
+        if a == 300:
             break
+        mfr = row[2]
+        #  print(mfr)
+        if mfr == "DODGE":
+            print(row)
 
