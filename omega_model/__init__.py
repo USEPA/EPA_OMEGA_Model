a0c7cbc9b25f7e6b3cd9ee7c84a9450443561975
kevinnewman <newman.kevin@epa.gov>
2023-03-09 14:55:31 -0500

    update default value

--- omega_model/__init__.py
+++ omega_model/__init__.py
@@ -199,7 +199,7 @@ try:
             self.producer_compliance_search_min_share_range = 1e-5
             self.producer_compliance_search_convergence_factor = 0.9
             self.producer_compliance_search_tolerance = 1e-6
-            self.producer_compliance_search_multipoint = False
+            self.producer_compliance_search_multipoint = True  # disable for zevregion batches
             self.producer_cross_subsidy_price_tolerance = 5e-3
             self.run_profiler = False
             self.multiprocessing = True and not self.run_profiler and not getattr(sys, 'frozen', False)
