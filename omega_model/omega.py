5fbe1c1d26092dae4c5c56d06379810b28918a7c
kevinnewman <newman.kevin@epa.gov>
2023-04-11 10:17:31 -0400

    cleanup

--- omega_model/omega.py
+++ omega_model/omega.py
@@ -312,8 +312,7 @@ def run_producer_consumer(pass_num, manufacturer_annual_data_table):
             iterate_producer_consumer = True
 
             if omega_globals.options.producer_shares_mode == 'auto':
-                # in order to hit the strategic offset we need to bypass the consumer a bit
-                # omega_globals.producer_shares_mode = omega_globals.pass_num == 1
+                # CU RV
                 omega_globals.producer_shares_mode = False
             elif omega_globals.options.producer_shares_mode is True:
                 omega_globals.producer_shares_mode = True
