e137b3a292bf17195b95f6daaf517478eb741941
kevinnewman <newman.kevin@epa.gov>
2023-04-30 12:55:36 -0400

    add effects executable build scripts

---
+++ versioning/build_effects_exe_dist_mac.command
@@ -0,0 +1,19 @@
+#! /bin/zsh
+
+cd ..
+
+# build executable
+
+pyinstaller omega_effects/omega_effects_main.py \
+  --name omega-effects-mac-20230428.command \
+  --paths omega_effects \
+  --noconfirm \
+  --onefile
+
+# cleanup
+
+mv *.spec versioning
+rm -R __pycache__
+rm -R build
+
+cd versioning
---
+++ versioning/build_effects_exe_dist_win.bat
@@ -0,0 +1,19 @@
+REM usage build_dist
+
+cd ..
+
+REM build executable
+
+pynstaller omega_effects/omega_effects_main.py ^
+  --name omega-effects-mac-20230428.command ^
+  --paths omega_effects ^
+  --noconfirm ^
+  --onefile
+
+REM cleanup
+
+move /Y  *.spec versioning
+rmdir /S /Q __pycache__
+rmdir /S /Q build
+
+cd versioning
