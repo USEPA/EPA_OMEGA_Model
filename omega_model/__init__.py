ec61f7a26047a762d4562c19b8ea74d59fdf8ada
kevinnewman <newman.kevin@epa.gov>
2023-06-08 17:23:42 -0400

    minor recalibration

--- omega_model/__init__.py
+++ omega_model/__init__.py
@@ -145,7 +145,7 @@ try:
 
             self.offcycle_credits_file = path + 'test_inputs/offcycle_credits.csv'
 
-            self.consumer_pricing_num_options = 4
+            self.consumer_pricing_num_options = 10
             self.consumer_pricing_multiplier_min = 1/1.1
             self.consumer_pricing_multiplier_max = 1.1
 
