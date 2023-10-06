90f1a9c079e01c98bb47c02f023762f17f30bd2b
toddsherwood-EPA <sherwood.todd@epa.gov>
2023-10-06 17:01:10 -0400

    Updates to refinery calcs. No more trying to calculate domestic refined gallons.

--- omega_effects/effects/vehicles.py
+++ omega_effects/effects/vehicles.py
@@ -461,7 +461,7 @@ class Vehicles:
             Nothing, but reads the appropriate input file.
 
         """
-        df = read_input_file(filepath, effects_log, index_col=0)
+        df = read_input_file(filepath, effects_log, index_col='vehicle_id')
 
         df = deregionalizer.deregionalize_entries(df, 'market_class_id', 'r1nonzev', 'r2zev')
         df = deregionalizer.deregionalize_entries(df, 'body_style', 'r1nonzev', 'r2zev')
