94f99ea071b48d5c00975f82b01a73cbfe4778ab
toddsherwood-EPA <sherwood.todd@epa.gov>
2023-12-29 16:06:09 -0500

    Bump version: 2023.12.2 → 2023.12.3

--- omega_effects/batch_settings.py
+++ omega_effects/batch_settings.py
@@ -49,7 +49,7 @@ class BatchSettings:
 
     """
     def __init__(self):
-        self.effects_package_version = '2023.12.2'
+        self.effects_package_version = '2023.12.3'
         self.runtime_info = None
         self.batch_df = pd.DataFrame()
         self.batch_program = None
