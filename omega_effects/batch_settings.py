5515cc768d4148e21e5d3da1cc7885a5320cbdca
toddsherwood-EPA <sherwood.todd@epa.gov>
2025-05-06 14:19:01 -0400

    Update effects package version number.

--- omega_effects/batch_settings.py
+++ omega_effects/batch_settings.py
@@ -360,7 +360,7 @@ class BatchSettings:
 
     """
     def __init__(self):
-        self.effects_package_version = '2025.3.1'
+        self.effects_package_version = '2025.5.0'
         self.start_time_readable = None
         self.branch_name = None
         self.runtime_info = None
